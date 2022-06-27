ac17a84e3bfdf6b4accec292a441deac3908c18f
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-06-27 19:41:38 -0300

    Responsividade - Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center" text-align="right">
-      <Box mr="4" >
-        <Text>Paulo Pimentel</Text>
-        <Text color="gray.300" fontSize="small">
-          pauloh.g.pimentel@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" >
+          <Text>Paulo Pimentel</Text>
+          <Text color="gray.300" fontSize="small">
+            pauloh.g.pimentel@gmail.com
+          </Text>
+        </Box>
+      )}
       
       <Avatar size="md" name="Paulo Pimentel" src="https://avatars.githubusercontent.com/u/25647587?v=4" />
     </Flex>
